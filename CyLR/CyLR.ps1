7276717b14571867c19b5c12357d08b63c9878fc
Vikas Singh <39370194+vikas891@users.noreply.github.com>
2021-11-14 18:43:23 +0530

    Create CyLR.ps1

---
+++ CyLR/CyLR.ps1
@@ -0,0 +1,12 @@
+#Download and extracts a fresh copy of CyLR to C:\Windows\Temp\Sophos_Cylr_[Timestamp]
+#We could check for C:\CyLR.exe and execute it directly but it's easier and safer to execute a fresh copy. 
+#Outputs a C:\Windows\Temp\Sophos_[Timestamp].[Hostname].zip which can be pulled using SDU Downloader. 
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+$Timestamp = (Get-Date -Format "yyyyMMddTHHmmss")
+$outpath = New-Item -Path "C:\windows\Temp\" -Name "Sophos_Cylr_$Timestamp" -ItemType "directory"
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/orlikoski/CyLR/releases/download/2.2.0/CyLR_win-x64.zip", "$outpath\CyLR_win-x64.zip")
+Expand-Archive -LiteralPath "$outpath\CyLR_win-x64.zip" -DestinationPath $outpath
+Start-Process -WindowStyle hidden -Filepath "$outpath\CyLR.exe" -ArgumentList "--usnjrnl -q -od C:\Windows\Temp -of Sophos_$TimeStamp.$env:computername.zip"
